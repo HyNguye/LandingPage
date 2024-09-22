73e1389522f9766ad39806a197577ce38462dcb9
HyNNA <rhychou1408@gmail.com>
2024-09-22 22:49:47 +0700

    complete edition full SEO score

--- master.js
+++ master.js
@@ -3,13 +3,14 @@ let avatars = [
     "./img/avatar04.jpg",
     "./img/avatar05.jpg",
     "./img/avatar06.jpg",
-    "./img/avatar07.jpg",
+    "./img/avatar07.png",
     "./img/avatar08.jpg",
     "./img/avatar09.jpg"
 ]
 let userMessage = ["vừa đặt mua 2 máy","Mình đặt combo 2 máy massage"," Vừa đặt 4 máy", "freeship luôn",'Máy tốt',"massage quá đã"];
 let userTime= ['Vừa xong', '5 phút trước', '1 phút trước', '4 phút trước' ,'3 phút trước','2 phút trước']
-
+let minute = 6;
+let second =33;
 function showToast() {
     const toast = new bootstrap.Toast(document.getElementById('customToast'));
     let fistIndex = getRandomNumber();
@@ -22,5 +23,14 @@ function showToast() {
 function getRandomNumber() {
   return Math.floor(Math.random() * 6); 
 }
-
+function countdown(){
+    if (minute >= 0 && second > 0) {
+        document.getElementById("second").textContent = second<=10?`0${--second}`:--second;
+        if (second===0 && minute!==0) {
+            document.getElementById("minutes").textContent = minute<=10?`0${--minute}`:--minute;
+            second = 60;
+        }
+    }
+}
   setInterval(showToast, 2000);
+  setInterval(countdown,1000);
